b04bfcea0203b74a627155a9f1d748e24f45f92b
Victor Nicolas Lizárraga Ochoa <15171141@itculiacan.edu.mx>
2020-08-30 23:35:02 -0600

    exceptions

--- packages/search-character-creators/queries/handlers/get-character-creator.handler.ts
+++ packages/search-character-creators/queries/handlers/get-character-creator.handler.ts
@@ -1,6 +1,7 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetCharacterCreatorByNameQuery } from "../impl/get-character-creator-by-name.query";
 import { ReadCharacterCreatorRepository } from "../../repositories/read-character-creator.repository";
+import { NotFoundException } from "@nestjs/common";
 
 @QueryHandler(GetCharacterCreatorByNameQuery)
 export class GetCharacterCreatorByNameHandler
@@ -8,6 +9,10 @@ export class GetCharacterCreatorByNameHandler
   constructor(private readonly repository: ReadCharacterCreatorRepository) {}
 
   async execute(query: GetCharacterCreatorByNameQuery) {
-    return await this.repository.get(query.name);
+    const characterCreators = await this.repository.get(query.name);
+    if (!characterCreators) {
+      throw new NotFoundException(`${query.name} character creators not found`);
+    }
+    return characterCreators;
   }
 }
--- packages/search-character-interactions/queries/handlers/get-character-interaction.handler.ts
+++ packages/search-character-interactions/queries/handlers/get-character-interaction.handler.ts
@@ -1,6 +1,7 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetCharacterInteractionByNameQuery } from "../impl/get-character-interaction-by-name.query";
 import { ReadCharacterInteractionsRepository } from "../../repositories/read-character-interactions.repository";
+import { NotFoundException } from "@nestjs/common";
 @QueryHandler(GetCharacterInteractionByNameQuery)
 export class GetCharacterInteractionByNameHandler
   implements IQueryHandler<GetCharacterInteractionByNameQuery> {
@@ -9,7 +10,12 @@ export class GetCharacterInteractionByNameHandler
   ) {}
 
   async execute(query: GetCharacterInteractionByNameQuery) {
-    const result = await this.repository.get(query.name);
-    return result;
+    const characterInteractions = await this.repository.get(query.name);
+    if (!characterInteractions) {
+      throw new NotFoundException(
+        `${query.name} character interactions not found`
+      );
+    }
+    return characterInteractions;
   }
 }
